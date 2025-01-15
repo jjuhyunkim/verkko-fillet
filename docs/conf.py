7461d136d4e2d9e7e0c78ea730222a68812bd7c6
Juhyun Kim <131193680+jjuhyunkim@users.noreply.github.com>
2025-01-15 18:55:48 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
 exclude_patterns = []  # List of patterns to ignore when looking for source files
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'alabaster'  # Choose a theme (default is 'alabaster')
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'  # Choose a theme (default is 'alabaster')
 #html_static_path = ['_static']  # Path to custom static files (e.g., CSS)
 
