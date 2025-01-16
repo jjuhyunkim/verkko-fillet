468cace29f1f0121a8328e5436a3c4c5325d6cd4
Juhyun Kim <131193680+jjuhyunkim@users.noreply.github.com>
2025-01-16 13:12:05 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,28 @@
+from __future__ import annotations
+
+import sys
+from datetime import datetime
+from functools import partial
+from pathlib import Path, PurePosixPath
+from typing import TYPE_CHECKING
+
+import matplotlib  # noqa
+from docutils import nodes
+from packaging.version import Version
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+HERE = Path(__file__).parent
+sys.path[:0] = [str(HERE.parent), str(HERE / "extensions")]
+
+# -- General configuration ------------------------------------------------
+
+nitpicky = True  # Warn about broken links. This is here for a reason: Do not change.
+needs_sphinx = "4.0"  # Nicer param docs
+suppress_warnings = [
+    "myst.header",  # https://github.com/executablebooks/MyST-Parser/issues/262
+]
 
 # Configuration file for the Sphinx documentation builder.
 
@@ -8,13 +30,32 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'Verkko-fillet'
 author = 'Juhyun Kim'
 release = '0.1'  # The full version, including alpha/beta/rc tags
+repository_url = "https://github.com/jjuhyunkim/verkko-fillet"
+copyright = f"{datetime.now():%Y}, the verkko-fillet development team"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',      # Include documentation from docstrings
-    'sphinx.ext.napoleon',     # Support for Google-style and NumPy-style docstrings
-    'sphinx.ext.viewcode',     # Add links to highlighted source code
-    'myst_parser',
+    "myst_nb",
+    "sphinx_copybutton",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "sphinxcontrib.bibtex",
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx_autodoc_typehints",  # needs to be after napoleon
+    "git_ref",  # needs to be before scanpydoc.rtd_github_links
+    "scanpydoc",  # needs to be before sphinx.ext.linkcode
+    "sphinx.ext.linkcode",
+    "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinx_search.extension",
+    "sphinxext.opengraph",
+    *[p.stem for p in (HERE / "extensions").glob("*.py") if p.stem not in {"git_ref"}],
 ]
 
 #templates_path = ['_templates']  # Path to custom HTML templates
